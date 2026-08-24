e3c5f6b74e936cdc44636d07a0385400666961e5
Cauã Carlos <93791778+ThebkrsLin@users.noreply.github.com>
2026-08-24 10:47:43 -0300

    pontos que faltavam foram inseridos
    - agora o VeiculoRepository relaciona os dados;
    - adicionado a tabela relacional

--- src/repository/VeiculoRepository.php
+++ src/repository/VeiculoRepository.php
@@ -1,5 +1,4 @@
 <?php
-
 class VeiculoRepository{
     private PDO $pdo;
 
@@ -8,8 +7,8 @@ class VeiculoRepository{
     }
 
     public function cadastrarVeiculo(Veiculo $veiculo): void{
-        $cadastrar = "INSERT INTO veiculos(placa, marca, modelo, ativo, ano, created_at, updated_at, motorista_id)
-        VALUES(:placa, :marca, :modelo, :ativo, :ano, :created_at, :updated_at, :motorista_id)";
+        $cadastrar = "INSERT INTO veiculos(placa, marca, modelo, ativo, ano, created_at, updated_at)
+        VALUES(:placa, :marca, :modelo, :ativo, :ano, :created_at, :updated_at)";
 
         $stmt = $this->pdo->prepare($cadastrar);
         $stmt->execute([
@@ -20,7 +19,6 @@ class VeiculoRepository{
             "ano"          => $veiculo->getAno(),
             "created_at"   => $veiculo->getCreatedAt(),
             "updated_at"   => $veiculo->getUpdatedAt(),
-            "motorista_id" => $veiculo->getMotoristaId()
         ]);
     }
 
@@ -37,9 +35,101 @@ class VeiculoRepository{
         }
 
         $veiculo = Veiculo::reconstruirVeiculo(
-            $res["id"], $res["placa"], $res["modelo"], $res["marca"], $res["ano"], $res["created_at"], $res["updated_at"], $res["ativo"], $res["motorista_id"] 
+            $res["id"], $res["placa"], $res["modelo"], $res["marca"], $res["ano"], $res["created_at"], $res["updated_at"], $res["ativo"]
         );
         return $veiculo;
+    }
+
+    public function buscarVeiculoPorMotorista(int $mid): ?Veiculo{
+        $buscar = "SELECT v.*
+               FROM veiculos v
+               INNER JOIN veiculo_motorista vm
+                   ON vm.veiculo_id = v.id
+               WHERE vm.motorista_id = :motorista_id
+               AND vm.data_fim IS NULL";
+
+        $stmt = $this->pdo->prepare($buscar);
+
+        $stmt->execute([
+            "motorista_id" => $mid
+        ]);
+
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($res === false){
+            return null;
+        }
+
+        return Veiculo::reconstruirVeiculo(
+            $res["id"],
+            $res["placa"],
+            $res["modelo"],
+            $res["marca"],
+            $res["ano"],
+            $res["created_at"],
+            $res["updated_at"],
+            $res["ativo"]
+        );
+    }
+
+    public function buscarPlaca(string $placa): ?Veiculo{
+
+        $sql = "SELECT * FROM veiculos
+                WHERE placa = :placa";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            "placa" => $placa
+        ]);
+
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($res === false){
+            return null;
+        }
+
+        return Veiculo::reconstruirVeiculo(
+            $res["id"],
+            $res["placa"],
+            $res["modelo"],
+            $res["marca"],
+            $res["ano"],
+            $res["created_at"],
+            $res["updated_at"],
+            $res["ativo"]
+        );
+    }
+
+    public function buscarMotoristaPorVeiculo(int $veiculoId): ?Motorista
+    {
+        $sql = "SELECT m.*
+                FROM motoristas m
+                INNER JOIN veiculo_motorista vm
+                    ON vm.motorista_id = m.id
+                WHERE vm.veiculo_id = :veiculo_id
+                AND vm.data_fim IS NULL";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            "veiculo_id" => $veiculoId
+        ]);
+
+        $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($res === false) {
+            return null;
+        }
+
+        return Motorista::reconstruirMotorista(
+            $res["id"],
+            $res["nome"],
+            $res["cpf"],
+            $res["ativo"],
+            $res["created_at"],
+            $res["updated_at"]
+        );
     }
 
     public function listarVeiculos(): array{
@@ -51,18 +141,48 @@ class VeiculoRepository{
 
         foreach($res as $v){
             $veiculos[] = Veiculo::reconstruirVeiculo(
-                $v["id"], $v["placa"], $v["modelo"], $v["marca"], $v["ano"], $v["created_at"], $v["updated_at"], $v["ativo"], $v["motorista_id"]
+                $v["id"], $v["placa"], $v["modelo"], $v["marca"], $v["ano"], $v["created_at"], $v["updated_at"], $v["ativo"]
             );
         }
 
         return $veiculos;
     }
 
+    public function criarAssociacao(int $veiculoId, int $motoristaId): void{
+        $sql = "INSERT INTO veiculo_motorista
+                (veiculo_id, motorista_id, data_inicio)
+                VALUES (:veiculo_id, :motorista_id, :data_inicio)";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            "veiculo_id" => $veiculoId,
+            "motorista_id" => $motoristaId,
+            "data_inicio" => date("Y-m-d H:i:s")
+        ]);
+    }
+
+    public function encerrarAssociacao(int $id, int $motorid): void{
+        $sql = "UPDATE veiculo_motorista
+                SET data_fim = :data_fim
+                WHERE veiculo_id = :veiculo_id
+                AND motorista_id = :motorista_id
+                AND data_fim IS NULL";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            "veiculo_id" => $id,
+            "motorista_id" => $motorid,
+            "data_fim" => date("Y-m-d H:i:s")
+        ]);
+    }
+
     public function atualizarVeiculo(Veiculo $veiculo): void{
         $atualizar= "UPDATE veiculos
         SET placa = :placa, modelo = :modelo, 
         marca = :marca, ano = :ano, ativo = :ativo, 
-        updated_at = :updated_at, motorista_id = :motorista_id
+        updated_at = :updated_at
         WHERE id = :id";
         $stmt = $this->pdo->prepare($atualizar);
         $stmt->execute([
@@ -73,7 +193,6 @@ class VeiculoRepository{
             "ano"          => $veiculo->getAno(),
             "ativo"        => $veiculo->getAtivo(),
             "updated_at"   => $veiculo->getUpdatedAt(),
-            "motorista_id" => $veiculo->getMotoristaId()
         ]);
     }
 }
